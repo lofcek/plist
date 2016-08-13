bd1370d17df122d3f4036deec8fdb330516a3a4f
lofcek <lofcek@gmail.com>
2016-08-13 23:33:24 +0200

    Bit more readable tests

--- plist_test.go
+++ plist_test.go
@@ -42,8 +42,10 @@ func TestUnmarshalPlist(t *testing.T) {
 	}
 	var s2 S2
 	var iface interface{}
+	//var piface *interface{}
 	var m1 map[string]interface{}
 	var m2 map[string]bool
+	var pm1 *map[string]interface{}
 
 	type TestUnmarshal struct {
 		name string
@@ -60,15 +62,15 @@ func TestUnmarshalPlist(t *testing.T) {
 		{"string_escape", `<string>&lt;&gt;</string>`, &s, UnmarshalExpectsEq{"<>"}},
 		{"unmarshalInt", `<integer>42</integer>`, &i, UnmarshalExpectsEq{int(42)}},
 		{"unmarshalInt64", `<integer>42</integer>`, &i64, UnmarshalExpectsEq{int64(42)}},
-		{"unmarshalOverflow", `<integer>256</integer>`, &i8, UnmarshalExpectsError{&strconv.NumError{}}},
-		{"wrong type", `<integer>256</integer>`, &s, UnmarshalExpectsError{&UnexpectedTokenError{}}},
+		{"unmarshalOverflow", `<integer>256</integer>`, &i8, UnmarshalExpectsError{(*strconv.NumError)(nil)}},
+		{"wrong type", `<integer>256</integer>`, &s, UnmarshalExpectsError{(*UnexpectedTokenError)(nil)}},
 		{"unmarshalUInt16", `<integer>10</integer>`, &u16, UnmarshalExpectsEq{uint16(10)}},
 		{"unmarshalUIntPtr", `<integer>10</integer>`, &up, UnmarshalExpectsEq{uintptr(10)}},
-		{"unmarshalToChan", `<integer>10</integer>`, new(chan int), UnmarshalExpectsError{&CannotParseTypeError{}}},
+		{"unmarshalToChan", `<integer>10</integer>`, new(chan int), UnmarshalExpectsError{(*CannotParseTypeError)(nil)}},
 		{"unmarshalFloat", `<real>3.14</real>`, &f32, UnmarshalExpectsEq{float32(3.14)}},
 		// spaces could be skipped, any not empty text should cause panic
 		{"true again", `<true/>  `, &b, UnmarshalExpectsEq{true}},
-		{"not empty space", `<true/>  aa`, &b, UnmarshalExpectsError{&UnexpectedTokenError{}}},
+		{"not empty space", `<true/>  aa`, &b, UnmarshalExpectsError{(*UnexpectedTokenError)(nil)}},
 
 		// special types like date or data
 		{"date", `<date>2016-05-04T03:02:01Z</date>`, &tm, UnmarshalExpectsEq{time.Date(2016, 5, 4, 3, 2, 1, 0, time.UTC)}},
@@ -76,7 +78,7 @@ func TestUnmarshalPlist(t *testing.T) {
 
 		// arrays
 		{"array", `<array><integer>4</integer><integer>2</integer></array>`, &ai, UnmarshalExpectsEq{[]int{4, 2}}},
-		{"wrong xml", `<array></integer>`, &ai, UnmarshalExpectsError{&xml.SyntaxError{}}},
+		{"wrong xml", `<array></integer>`, &ai, UnmarshalExpectsError{(*xml.SyntaxError)(nil)}},
 		{"comments", ` <!-- use spaces and comments inside--> <array><!-- --><real>4</real> <real>2</real><!-- --> </array> <!-- -->`, &af32, UnmarshalExpectsEq{[]float32{4, 2}}},
 		{"pointer", `<integer>4</integer>`, &pi, UnmarshalExpectsEq{&i4}},
 
@@ -86,7 +88,7 @@ func TestUnmarshalPlist(t *testing.T) {
 		{"tags", `<dict><key>B</key><integer>1</integer><key>A</key><integer>2</integer><key>C</key><integer>3</integer></dict>`, &s2, UnmarshalExpectsEq{S2{B: 1, C: 2, A: 3, D: 0}}},
 		{"dict, omit field", `<dict><key>B</key><integer>1</integer><key>A</key><integer>2</integer><key>C</key><integer>3</integer><key>D</key><integer>5</integer></dict>`, &s2, UnmarshalExpectsEq{S2{B: 1, C: 2, A: 3, D: 0}}},
 		{"empty dict", `<dict></dict>`, &s1, UnmarshalExpectsEq{S1{0, false}}},
-		{"wrong tag", `<not-dict></not-dict>`, &s1, UnmarshalExpectsError{&UnexpectedTokenError{}}},
+		{"wrong tag", `<not-dict></not-dict>`, &s1, UnmarshalExpectsError{(*UnexpectedTokenError)(nil)}},
 		{"xml directive", `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE plist SYSTEM "file://localhost/System/Library/DTDs/PropertyList.dtd"><true/>`, &b, UnmarshalExpectsEq{true}},
 
 		// decode into interface{}
@@ -99,6 +101,8 @@ func TestUnmarshalPlist(t *testing.T) {
 		{"iface_map_iface", `<dict><key>x</key><true/><key>y</key><false/></dict>`, &m1, UnmarshalExpectsEq{map[string]interface{}{"x": true, "y": false}}},
 		{"iface_map_bool", `<dict><key>x</key><true/><key>y</key><false/></dict>`, &m2, UnmarshalExpectsEq{map[string]bool{"x": true, "y": false}}},
 		{"dictToIface", `<dict><key>x</key><true/><key>y</key><false/></dict>`, &iface, UnmarshalExpectsEq{map[string]interface{}{"x": true, "y": false}}},
+		//{"dictToPIface", `<dict><key>x</key><true/><key>y</key><false/></dict>`, &piface, UnmarshalExpectsEq{&map[string]interface{}{"x": true, "y": false}}},
+		{"pmap_iface", `<dict><key>x</key><true/><key>y</key><false/></dict>`, &pm1, UnmarshalExpectsEq{&map[string]interface{}{"x": true, "y": false}}},
 	}
 
 	for _, c := range test_cases {
@@ -139,7 +143,9 @@ func (res UnmarshalExpectsEq) TestUnmarshal(t *testing.T, xml_text string, v int
 }
 
 type UnmarshalExpectsError struct {
-	errType error
+	errType interface{
+		error
+	}
 }
 
 func (exp UnmarshalExpectsError) TestUnmarshal(t *testing.T, xml_text string, v interface{}) {
